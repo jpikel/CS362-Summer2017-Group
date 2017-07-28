58ba1d7550a947b83a15891c8203f6a4e6b53842
jpikel <pikelj@oregonstate.edu>
2017-07-28 11:42:55 +0000

    programming based testing initial commit
    Created a test routine following the programming based testing "Third"

--- URLValidatorInCorrect/bin/README.md
+++
@@ -1,5 +0,0 @@
-Final Project:
-
-Part-B: Due date is Thursday, August 17th at 23:59 pm (PST).
-
-
---
+++ URLValidatorInCorrect/src/TestURLprogramming.java
@@ -0,0 +1,88 @@
+/* Filename: TestURLprogramming
+ * Group Members: Akshay Subramanian, Edward Francis, Johannes Pikel
+ * Class: CS362-400
+ * Date: 2017.07.28
+ * Assignmnet: Final Project Part B
+ */
+
+//import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+
+
+
+import org.junit.Test;
+
+public class TestURLprogramming {
+	
+	/* this is the list of possible URL components that we will put together */
+	/* also referenced https://tools.ietf.org/html/rfc3986 */
+	/* possible Schemes taken from https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml*/
+	 ResultPair[] schemes = { new ResultPair("http://", true),
+		 					new ResultPair("imap://", true),
+		 					new ResultPair(":/", false)};
+	 
+	 	/* host name cite https://en.wikipedia.org/wiki/Hostname */
+	 ResultPair[] hostnames = { new ResultPair("www.google.com", true),
+		 					new ResultPair("osu.edu", true),
+		 					new ResultPair("#awesome.org", false)};
+	 
+	 ResultPair[] ports = { new ResultPair(":80", true),
+		 				new ResultPair(":81", true),
+		 				new ResultPair(":-80", false)};
+	 
+	 ResultPair[] paths = { new ResultPair("/file/img123.png", true),
+		 				new ResultPair("", true),
+		 				new ResultPair("/file^/img123.png", false)};
+	 
+	 ResultPair[] queries = {	new ResultPair("", true),
+			 				new ResultPair("?name=ferret", true),
+		 				new ResultPair("?name=ferret&age=23", true)};
+	
+	ResultPair[][] options = {schemes, hostnames, ports, paths, queries}; 
+	
+	UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES); 
+	
+	@Test
+	public void test() {
+		/* three static tests taken from the examples in URLValidatorTest.java */
+		boolean result;
+		assertTrue(urlVal.isValid("http://www.google.com"));
+		assertTrue(urlVal.isValid("http://www.google.com/"));
+		/* this unit test always fails! */
+		//assertTrue(urlVal.isValid("http://www.google.com:80/test1?action=view"));
+   	
+    	int totalPerms = schemes.length * hostnames.length * ports.length * paths.length * queries.length;
+    	int[] counters = {schemes.length, hostnames.length, ports.length, paths.length, queries.length};
+    	int[] positions = {0,0,0,0,0};
+    	for (int i = 0; i < totalPerms; i ++) {
+    	    boolean expectedResult = true;
+    	    StringBuffer urlBuffer = new StringBuffer();
+    		for (int j = 0; j < counters.length; j++) {
+    			if (positions[j] >= counters[j]) {
+    				positions[j] = 0;
+					if (j-1 > 0 ) {
+						positions[j-1] += 1;
+					}
+    			}
+    			if (options[j][positions[j]].valid == false) {
+    				expectedResult = false;
+    			}
+    			urlBuffer.append(options[j][positions[j]].item);
+    		}
+        	String urlString = urlBuffer.toString();
+    		result = urlVal.isValid(urlString);
+    		String format = "%-70s%-30s";
+    		System.out.printf(format, urlString, "Expect: " + expectedResult + " Returned: " + result);
+    		if (result != expectedResult) {
+    			System.out.print("\t TEST FAILED!");
+    		}
+    		System.out.println();
+    		positions[counters.length - 1] += 1;
+    	}
+    	
+	}
+	
+
+}
+ 
